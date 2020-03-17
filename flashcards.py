62981cf2fc0f7cf505326e3c1a2500cb2be724f7
AliDenno <aliedenno@gmail.com>
2020-03-17 13:12:42 +0100

    add date() and count_views()

--- flashcards.py
+++ flashcards.py
@@ -1,8 +1,22 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
+counter = 0
 
 
 @app.route("/")
 def welcome():
     return "Welcome to my Flash Cards Application!"
+
+
+@app.route("/date")
+def date():
+    return "This page is being served at " + str(datetime.now())
+
+
+@app.route("/count_views")
+def count_views():
+    global counter
+    counter += 1
+    return "This page was served " + str(counter) + " times"
